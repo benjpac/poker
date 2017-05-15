58df28ef5a1667f3068ac385430a98ad3862222d
Ben Metzger and Jason Lazzuri <ben+jason@>
2017-05-15 16:31:51 -0700

    add tests for database relationships

--- lib/flop.rb
+++ lib/flop.rb
@@ -1,3 +1,12 @@
 class Flop < ActiveRecord::Base
   has_many :cards
+
+  def self.deal_flop
+    flop = []
+    for i in 1..3
+      flop.push(Card.deal_card)
+    end
+    return flop
+  end
+
 end
