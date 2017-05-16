4c30e00766b1bf8ec4a7bbf33f67739944281b55
Ben Metzger <benjpac@gmail.com>
2017-05-16 14:32:19 -0700

    add backend to have pull cards for 1 player

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,26 @@
 class Round < ActiveRecord::Base
   has_many :cards
   belongs_to :deck
-  # after_create(:create_flop)
-  #
-  # def create_flop
-  #   deck = self.deck
-  #   for i in 1..3
-  #     random_card = deck.pull_random_card
-  #     random_card.update(round_id: self.id)
-  #   end
-  # end
+  after_create(:create_flop)
+
+  def create_flop
+    deck = self.deck
+    for i in 1..3
+      random_card = deck.pull_random_card
+      random_card.update(round_id: self.id)
+    end
+  end
+
+  def create_turn
+    deck = self.deck
+    random_card = deck.pull_random_card
+    random_card.update(round_id: self.id)
+  end
+
+  def create_river
+    deck = self.deck
+    random_card = deck.pull_random_card
+    random_card.update(round_id: self.id)
+  end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -7,6 +7,11 @@ describe Round do
   it 'returns 3 cards from deck and assigns round_id' do
     deck = Deck.create
     round = deck.create_round
+    hand1 = deck.hands.create
+    deck.round.create_turn
+    deck.round.create_river
+    test1 = hand1.add_round_id(round)
     binding.pry
+    # expect(deck.cards.where(round_id: round.id)).to all(include({"deck_id" => deck.id}))
   end
 end
