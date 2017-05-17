abf4ca0f9f7ac3a295c366b3ef2ed57705016c68
Ben Metzger <benjpac@gmail.com>
2017-05-17 15:20:13 -0700

    more stylin

--- app.rb
+++ app.rb
@@ -9,42 +9,28 @@ get '/' do
   erb :index
 end
 
-get '/how_many_players' do
-  erb :how_many_players
+post '/player_names' do
+  erb :player_names
+end
+
+post '/play_game' do
+  round = Round.create(pot: 0)
+  player1 = Player.create(name: params[:player1], money: 99, round_id: round.id)
+  player2 = Player.create(name: params[:player2], money: 98, round_id: round.id)
+  Card.create_deck
+  hand1 = Hand.create(bet: 1, player_id: player1.id)
+  hand1.create_hand
+  hand2 = Hand.create(bet: 2, player_id: player2.id)
+  hand2.create_hand
+  round.update(pot: 0, active_player_id: player1.id)
+  redirect '/text/2_player/round/'.concat(round.id.to_s)
 end
 
 get '/deal' do
   erb :deal
 end
 
-get '/2_player_deal' do
-  Card.create_deck
-  @hand = Hand.create
-  @hand.create_hand
-  @round = Round.create
-  @card_urls = []
-  @round.cards.each do |card|
-    @card_urls.push(card.image_string)
-  end
-  erb :two_player_deal
-end
-
-
-
-
-get '/text' do
-  erb :text
-end
-
-get '/text/form' do
-  erb :text_form
-end
-
-post '/text/form' do
-  redirect '/text/form'
-end
-
-post '/text/players' do
+post '/player_names' do
   round = Round.create(pot: 0)
   player1 = Player.create(name: params[:player1], money: 99, round_id: round.id)
   player2 = Player.create(name: params[:player2], money: 98, round_id: round.id)
